0528a6d59334b756a96350e85237d89826730d0a
Nadav Spitzer <nspitzern@gmail.com>
2019-10-16 12:55:10 +0300

    added q,r keys effects. fixed proportions in instructions and main menu screens. items in instructions not showing

---
+++ learning_project/Assets/KeyboardListener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyboardListener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            SceneManager.LoadScene("main_menu");
+        }
+        else if (Input.GetKey(KeyCode.R) && SceneManager.GetActiveScene().name == "SampleScene") {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
