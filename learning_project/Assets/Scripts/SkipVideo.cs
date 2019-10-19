dc929103205efb87563e55e26989fee901210e02
teddy <lazebnik.teddy@gmail.com>
2019-10-19 17:44:02 +0300

    fixes to the game flow

---
+++ learning_project/Assets/Scripts/SkipVideo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipVideo : MonoBehaviour
+{
+    public void GotoStoryScene()
+    {
+        SceneManager.LoadScene("Story");
+    }
+}
