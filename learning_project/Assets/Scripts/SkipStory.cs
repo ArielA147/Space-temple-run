b9c519c824f8be75f851c123f37f8db219aa7b84
teddy <lazebnik.teddy@gmail.com>
2019-10-19 18:05:06 +0300

    changes for ariel

---
+++ learning_project/Assets/Scripts/SkipStory.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipStory : MonoBehaviour
+{
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("main_menu");
+    }
+}
