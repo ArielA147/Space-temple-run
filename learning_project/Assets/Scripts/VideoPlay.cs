c5a30a945363d3c593c52d69be9d8f90dd635c3a
Nadav Spitzer <nspitzern@gmail.com>
2019-10-20 12:13:05 +0300

    added webGL build folder

--- learning_project/Assets/Scripts/VideoPlay.cs
+++ learning_project/Assets/Scripts/VideoPlay.cs
@@ -10,16 +10,19 @@ public class VideoPlay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
-        var audioSource = gameObject.AddComponent<AudioSource>();
+        var videoPlayer = gameObject.GetComponent<UnityEngine.Video.VideoPlayer>();
+        // var videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
+        // var audioSource = gameObject.AddComponent<AudioSource>();
 
-        videoPlayer.playOnAwake = false;
-        videoPlayer.clip = videoClip;
-        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
-        videoPlayer.targetMaterialRenderer = GetComponent<Renderer>();
-        videoPlayer.targetMaterialProperty = "_MainTex";
-        videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
-        videoPlayer.SetTargetAudioSource(0, audioSource);
+        // videoPlayer.playOnAwake = false;
+        // videoPlayer.clip = videoClip;
+        // videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+        // videoPlayer.targetMaterialRenderer = GetComponent<Renderer>();
+        // videoPlayer.targetMaterialProperty = "_MainTex";
+        // videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
+        // videoPlayer.SetTargetAudioSource(0, audioSource);
+        videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,"Guardians of the Galaxy Prison Fight.mp4"); 
+        videoPlayer.Play();
     }
 
     // Update is called once per frame
